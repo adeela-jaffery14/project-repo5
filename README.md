32185caf7be4907ef7961f4eb06fdc4472dfb787
addi110 <addi110@gmail.com>
2024-03-13 23:58:43 -0500

    Completed JS and corrected html plus css

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Demo Project# 5
 
-Need to add description after I complete the project.
+This project, I have learned on how to make an image appear blurry in the beginning to turning fully clear. The opposite for the loading percentage and its div. 
+
+Thank you for viewing this project. 
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const loadText = document.querySelector('.loading-text')
 const picture = document.querySelector('.blur')
+const textDive = document.querySelector('.text-div')
 
 let load = 0
 
@@ -13,6 +14,13 @@ function blurring() {
     }
     
     loadText.innerText = `${load}%`
-    console.log(load)
-    console.log(loadText)
+    loadText.style.opacity = scale(load, 0, 100, 1, 0)
+    textDive.style.opacity = scale(load, 0, 100, 1, 0)
+    picture.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
+
